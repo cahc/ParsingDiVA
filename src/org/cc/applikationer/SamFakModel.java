f4a9365ca13e04c3355eb04d1668a498230f380f
cahc.ub <cristian.colliander@ub.umu.se>
2017-09-04 17:07:46 +0200

    Added SF-model

--- src/org/cc/applikationer/SamFakModel.java
+++ src/org/cc/applikationer/SamFakModel.java
@@ -26,7 +26,7 @@ public class SamFakModel {
     public static void main(String[] arg) throws IOException, XMLStreamException, ParseException {
 
 
-        if(arg.length != 3) { System.out.println(" java -cp .. norwegianList.xlsx thesaurusFile.xlsx divaDump.csv"); System.exit(0); }
+        if(arg.length != 5) { System.out.println(" java -cp ... norwegianList.xlsx thesaurusFile.xlsx divaDump.csv AffiliationMappingFile.xlsx PersonalData.xml"); System.exit(0); }
 
 
        File auktoritetsRegisterFil = new File( arg[0] );
@@ -68,7 +68,7 @@ public class SamFakModel {
 
         //ConsideredPublications defaultPubInclusion = new DefaultPubInclusion();
 
-        ConsideredPublications consideredPublications = new SFPubInclusion();
+        ConsideredPublications sfPubInclusion = new SFPubInclusion();
 
         List<Post> postList = new ArrayList<>(100);
 
@@ -106,34 +106,9 @@ public class SamFakModel {
 
       //  AuthorDisambiguation authorDisambiguation = new AuthorDisambiguation(); //constructor with hardcoded paths to personalData.xml and Mappings.excel
 
-        AuthorDisambiguation authorDisambiguation = new AuthorDisambiguation(new java.io.File("Mappningsfil20170807.xlsx"), new File("PersonalData.xml"));
+        AuthorDisambiguation authorDisambiguation = new AuthorDisambiguation(new java.io.File(arg[3]), new File(arg[4]));
 
         authorDisambiguation.mapAffiliationsAndDisanbigueAuthors(postList);
-
-
-
-
-     /*   System.out.println("Printing authors..");
-
-
-        for(Post p : postList) {
-
-            List<Author> authorList = p.getAuthorList();
-
-
-                for(Author a : authorList) {
-
-                    System.out.println( p.getPID() + "\t" + a.getDisambiguateID() + "\t" + a.getAuthorName() +"\t" + a.getCas() +"\t" + a.getAutomaticAddedCass() +"\t" + a.getFractionIgnoreMultipleUmUAffils() + "\t" + a.getFractionConsiderMultipleUmUAffils() +"\t" +a.getNrUmUaddresses() + "\t" +a.getAffiliations() +"\t" +a.getAffilMappingsObjects() );
-                }
-
-
-        }
-
-
-        System.exit(0);
-
-
-*/
 
 
 
@@ -143,7 +118,7 @@ public class SamFakModel {
 
 
 
-            p.setStatusInModel( consideredPublications.consideredPub(p) ); // ska publikationen matchas mot norska listan, se StatusInMdelConstants
+            p.setStatusInModel( sfPubInclusion.consideredPub(p) ); // ska publikationen matchas mot norska listan, se StatusInMdelConstants
             NorwegianMatchInfo matchInfo = NorskNivå.getNorwegianLevel(listaMedSerier,listaMedFörlag,p,0.9,standardiseringsListor); //matcha eventuellt mot norska listan samt uppdatera StatusInModel
             p.setNorskNivå( matchInfo ); // uppdatera posten med information om matchning mot norska listan
 
@@ -157,30 +132,6 @@ public class SamFakModel {
 
 
 
-/*
-        System.out.println("OK.. saving to file..");
-
-        BufferedWriter writer = new BufferedWriter( new FileWriter( new File("resultDebugg.txt")));
-
-        for(Post p :postList) {
-
-            for(Author a : p.getAuthorList() ) {
-
-
-                writer.write(a.printMappedAuthorWithFractionsAndNorwegianModel());
-                writer.newLine();
-            }
-
-
-        }
-
-
-        writer.flush();
-        writer.close();
-       System.out.println("OK.. now saving to Excel..");
-
-*/
-
         SaveToExcel saveToExcel = new SaveToExcel();
         saveToExcel.saveNorwegianMatchingInfo(postList);
 
@@ -190,7 +141,7 @@ public class SamFakModel {
        saveToExcel = new SaveToExcel();
         saveToExcel.saveDisambiguatedAuthorFractions(postList,true);
 
-        System.out.println("Resultat sparat i två Excel-filer: Fractions och NorskMatchning..");
+        System.out.println("Resultat sparat i tre Excel-filer: Fractions och NorskMatchning..");
 
         }
 
